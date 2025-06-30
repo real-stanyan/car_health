4614cd78a7473cf96ab4dafff7f22259e8604e2c
Stan Yan <stanhavenoidea@gmail.com>
2025-06-30 14:52:58 +1000

    env init

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,13 +9,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6ciyIZkj3PWS_rYyw6AKAi6gwYjB2lpM",
-  authDomain: "car-health-49cc0.firebaseapp.com",
-  projectId: "car-health-49cc0",
-  storageBucket: "car-health-49cc0.firebasestorage.app",
-  messagingSenderId: "414727124956",
-  appId: "1:414727124956:web:0dbd6c3990ac46140a4119",
-  measurementId: "G-9JYRQRPF4D",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
